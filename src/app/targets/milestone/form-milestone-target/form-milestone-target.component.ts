fd4ba16369856bc7557bbd6550a30d6873b92b57
Orlando Quero <orlaqp@binaryways.com>
2018-09-16 08:35:05 -0400

    reset milestone when it is not new and do not remove it when cancel the form

--- src/app/targets/milestone/form-milestone-target/form-milestone-target.component.ts
+++ src/app/targets/milestone/form-milestone-target/form-milestone-target.component.ts
@@ -1,17 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormGroupTypeSafe } from '../../../shared/services';
 import { IMilestone } from '../../shared/models/targets.model';
 import { TargetScreenService } from '../../shared/services/target-screen.service';
 import { IListItem } from '../../../shared/ui/lists/list-item';
-
-const usersQueryGql = require('graphql-tag/loader!./users.query.gql');
 
 @Component({
   selector: 'kpi-form-milestone-target',
   templateUrl: './form-milestone-target.component.pug',
   styleUrls: ['./form-milestone-target.component.scss'],
 })
-export class FormMilestoneTargetComponent {
+export class FormMilestoneTargetComponent implements OnInit {
     @Input()
     fg: FormGroupTypeSafe<IMilestone>;
     @Input()
@@ -19,17 +17,24 @@ export class FormMilestoneTargetComponent {
     @Input()
     index: number;
 
+    private milestone: IMilestone;
+
     constructor(private targetService: TargetScreenService) { }
+
+    ngOnInit() {
+        this.milestone = this.fg.value;
+    }
 
     save() {
         this.fg.markAsPristine();
     }
 
     cancel() {
-        this.fg.reset();
-
-        if (!this.fg.valid) {
+        if (!this.milestone.task) {
             this.targetService.removeMilestone(this.index);
+        } else {
+            this.fg.setValue(this.milestone);
+            this.save();
         }
     }
 }
