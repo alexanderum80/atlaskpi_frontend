442a4792750b4bc952202c5f36302f01928f1bf6
Beatriz Hernandez <bhern104@fiu.edu>
2018-10-05 09:56:19 -0400

    initial commit add dash to widget form no compilation errors

--- src/app/widgets/widget-form/widget-form.component.ts
+++ src/app/widgets/widget-form/widget-form.component.ts
@@ -128,6 +128,7 @@ export class WidgetFormComponent implements OnInit, AfterViewInit, OnDestroy {
     @Input() widgetId: string;
     @Input() widgetDataFromKPI : any;
     @Output() formResult = new EventEmitter < DialogResult > ();
+    @Output() newComponentResult = new EventEmitter<string>();
 
     subs: Subscription[] = [];
     datePickerConfig: IDatePickerConfig;
@@ -144,6 +145,7 @@ export class WidgetFormComponent implements OnInit, AfterViewInit, OnDestroy {
 
     dateRangeSelectionList: SelectionItem[] = [];
     kpiSelectionList: SelectionItem[] = [];
+    dashboardSelectionList: SelectionItem[] = [];
     chartSelectionList: SelectionItem[] = [];
     comparisonSelectionList: SelectionItem[] = [];
 
@@ -293,6 +295,7 @@ export class WidgetFormComponent implements OnInit, AfterViewInit, OnDestroy {
         this.subs.push(this._widgetFormService.chartList$.subscribe(list => this.chartSelectionList = list));
         this.subs.push(this._widgetFormService.dateRangeList$.subscribe(list => this.dateRangeSelectionList = list));
         this.subs.push(this._widgetFormService.kpiList$.subscribe(list => this.kpiSelectionList = list));
+        this.subs.push(this._widgetFormService.dashboardList$.subscribe(list => this.dashboardSelectionList = list));
         this.subs.push(this._widgetFormService.widgetModelValid$.subscribe(isValid => this.widgetModelValid = isValid));
     }
 
