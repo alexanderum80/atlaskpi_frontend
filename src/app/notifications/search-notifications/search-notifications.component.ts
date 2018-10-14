e5ebb795b1324ab1f4a7c02efd7c2d58deab5d69
Orlando Quero <orlaqp@binaryways.com>
2018-10-14 12:42:22 -0400

    added notifications page

--- src/app/notifications/search-notifications/search-notifications.component.ts
+++ src/app/notifications/search-notifications/search-notifications.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { INotification } from '../shared/models/notification';
 
 @Component({
-  selector: 'kpi-search-notifications',
-  templateUrl: './search-notifications.component.html',
-  styleUrls: ['./search-notifications.component.scss']
+    selector: 'kpi-search-notifications',
+    templateUrl: './search-notifications.component.pug',
+    styleUrls: ['./search-notifications.component.scss'],
 })
 export class SearchNotificationsComponent implements OnInit {
+    notifications: INotification[];
 
-  constructor() { }
+    constructor() {
+        // TODO: just for development
+        this.notifications = [
+            { deliveryMethod: 'email', timestamp: new Date(), message: 'Message 1' },
+            { deliveryMethod: 'push', timestamp: new Date(), message: 'Message 2' },
+            { deliveryMethod: 'push', timestamp: new Date(), message: 'Message 3' },
+            { deliveryMethod: 'email', timestamp: new Date(), message: 'Message 4' },
+            { deliveryMethod: 'email', timestamp: new Date(), message: 'Message 5' },
+            { deliveryMethod: 'push', timestamp: new Date(), message: 'Message 6' },
+            { deliveryMethod: 'email', timestamp: new Date(), message: 'Message 7' },
 
-  ngOnInit() {
-  }
+        ];
+    }
 
+    ngOnInit() {}
 }
---
+++ src/app/notifications/shared/models/notification.ts
@@ -0,0 +1,5 @@
+export interface INotification {
+    deliveryMethod: string;
+    timestamp: Date;
+    message: string;
+}
