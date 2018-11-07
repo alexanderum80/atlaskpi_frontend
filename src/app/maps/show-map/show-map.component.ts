b792c1817a3c4a91f53ac0c1f5e2f1d90dfb33a3
Beatriz Hernandez <bhern104@fiu.edu>
2018-11-06 21:58:24 -0500

    adding more dark styles

--- src/app/maps/show-map/show-map.component.ts
+++ src/app/maps/show-map/show-map.component.ts
@@ -12,6 +12,7 @@ import { Subscription } from 'rxjs/Subscription';
 import {objectWithoutProperties} from '../../shared/helpers/object.helpers';
 import {CommonService} from '../../shared/services/common.service';
 import { Store } from 'src/app/shared/services';
+import { style_dark } from './dark-map-styles';
 
 const mapMarkerQuery = require('graphql-tag/loader!./map-markers.query.gql');
 
@@ -40,239 +41,7 @@ export class ShowMapComponent implements OnChanges, OnDestroy {
     lat: number;
     lng: number;
     style = [];
-    style_dark = [
-        {
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#1d2c4d"
-            }
-          ]
-        },
-        {
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#8ec3b9"
-            }
-          ]
-        },
-        {
-          "elementType": "labels.text.stroke",
-          "stylers": [
-            {
-              "color": "#1a3646"
-            }
-          ]
-        },
-        {
-          "featureType": "administrative.country",
-          "elementType": "geometry.stroke",
-          "stylers": [
-            {
-              "color": "#4b6878"
-            }
-          ]
-        },
-        {
-          "featureType": "administrative.land_parcel",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#64779e"
-            }
-          ]
-        },
-        {
-          "featureType": "administrative.province",
-          "elementType": "geometry.stroke",
-          "stylers": [
-            {
-              "color": "#4b6878"
-            }
-          ]
-        },
-        {
-          "featureType": "landscape.man_made",
-          "elementType": "geometry.stroke",
-          "stylers": [
-            {
-              "color": "#334e87"
-            }
-          ]
-        },
-        {
-          "featureType": "landscape.natural",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#023e58"
-            }
-          ]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#283d6a"
-            }
-          ]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#6f9ba5"
-            }
-          ]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "labels.text.stroke",
-          "stylers": [
-            {
-              "color": "#1d2c4d"
-            }
-          ]
-        },
-        {
-          "featureType": "poi.park",
-          "elementType": "geometry.fill",
-          "stylers": [
-            {
-              "color": "#023e58"
-            }
-          ]
-        },
-        {
-          "featureType": "poi.park",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#3C7680"
-            }
-          ]
-        },
-        {
-          "featureType": "road",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#304a7d"
-            }
-          ]
-        },
-        {
-          "featureType": "road",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#98a5be"
-            }
-          ]
-        },
-        {
-          "featureType": "road",
-          "elementType": "labels.text.stroke",
-          "stylers": [
-            {
-              "color": "#1d2c4d"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#2c6675"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry.stroke",
-          "stylers": [
-            {
-              "color": "#255763"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#b0d5ce"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "labels.text.stroke",
-          "stylers": [
-            {
-              "color": "#023e58"
-            }
-          ]
-        },
-        {
-          "featureType": "transit",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#98a5be"
-            }
-          ]
-        },
-        {
-          "featureType": "transit",
-          "elementType": "labels.text.stroke",
-          "stylers": [
-            {
-              "color": "#1d2c4d"
-            }
-          ]
-        },
-        {
-          "featureType": "transit.line",
-          "elementType": "geometry.fill",
-          "stylers": [
-            {
-              "color": "#283d6a"
-            }
-          ]
-        },
-        {
-          "featureType": "transit.station",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#3a4762"
-            }
-          ]
-        },
-        {
-          "featureType": "water",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#0e1626"
-            }
-          ]
-        },
-        {
-          "featureType": "water",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#4e6d70"
-            }
-          ]
-        }
-      ];
+
     showingLegend = true;
 
     showingMapSettings = false;
@@ -367,12 +136,10 @@ export class ShowMapComponent implements OnChanges, OnDestroy {
     }
 
     checkAppTheme(state){
-
         // Check theme in app state
-        debugger;
         console.log(state);
     if(state.theme == 'dark'){
-            this.style = this.style_dark;
+            this.style = style_dark;
         }else{
             this.style = []; 
         }
