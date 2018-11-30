e67e6e4d3e9fa702369407aa7220076c4ad0a0db
Orlando Quero <orlaqp@gmail.com>
2018-11-30 13:44:46 -0500

    change the way to redirect the user to a page from the mobile home page

--- src/app/mobile-welcome/mobile-welcome.component.ts
+++ src/app/mobile-welcome/mobile-welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mobile-welcome',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MobileWelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router) { }
 
   ngOnInit() {
+  }
+
+  navigateTo(address: string) {
+    this._router.navigate([address]);
   }
 
 }
