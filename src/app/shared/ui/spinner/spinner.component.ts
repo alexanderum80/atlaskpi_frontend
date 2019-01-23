c0519528c76091c5635eddf55fb5902678770e9e
Orlando Quero <orlaqp@gmail.com>
2019-01-23 10:39:09 -0500

    added loading screen" "

--- src/app/shared/ui/spinner/spinner.component.ts
+++ src/app/shared/ui/spinner/spinner.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { BrowserService, IViewportSize } from '../../services/browser.service';
+import { Store } from '../../services';
 
 @Component({
   selector: 'kpi-spinner',
   styleUrls: ['./spinner.component.scss'],
   template: `
-  <div class="spinner-container">
+  <div class="spinner-container" [style.left.px]="left">
     <div class="sk-fading-circle spinner">
         <div class="sk-circle sk-circle1"></div>
         <div class="sk-circle sk-circle2"></div>
@@ -22,4 +24,18 @@ import { Component, OnInit } from '@angular/core';
   </div>
   `
 })
-export class SpinnerComponent { }
+export class SpinnerComponent {
+
+    left: number;
+
+    constructor(browser: BrowserService) {
+        browser.viewportSize$.subscribe(s => {
+            this.updateLeftValue(s);
+        });
+    }
+
+    private updateLeftValue(size: IViewportSize) {
+        this.left = size.width >= 1200 ? 220 : 0;
+    }
+
+}
