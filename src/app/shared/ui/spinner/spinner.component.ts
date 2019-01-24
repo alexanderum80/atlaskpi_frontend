1f052cec9eb9a0b91501ff7511b0afcb4f9d7a55
Orlando Quero <orlaqp@gmail.com>
2019-01-24 16:59:54 -0500

    fixed spinner position

--- src/app/shared/ui/spinner/spinner.component.ts
+++ src/app/shared/ui/spinner/spinner.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { BrowserService, IViewportSize } from '../../services/browser.service';
 import { Store } from '../../services';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'kpi-spinner',
   styleUrls: ['./spinner.component.scss'],
   template: `
   <div class="spinner-container" [style.left.px]="left">
-    <div class="spinner">
+    <div class="spinner" [style.left]="leftSpinner">
         <img src="/assets/img/loading.gif"/>
     </div>
   </div>
@@ -16,8 +17,12 @@ import { Store } from '../../services';
 export class SpinnerComponent {
 
     left: number;
+    leftSpinner: SafeStyle;
 
-    constructor(browser: BrowserService) {
+    constructor(
+        browser: BrowserService,
+        private sanitizer: DomSanitizer,
+    ) {
         browser.viewportSize$.subscribe(s => {
             this.updateLeftValue(s);
         });
@@ -25,6 +30,7 @@ export class SpinnerComponent {
 
     private updateLeftValue(size: IViewportSize) {
         this.left = size.width >= 1200 ? 220 : 0;
+        this.leftSpinner = this.sanitizer.bypassSecurityTrustStyle(size.width >= 1200 ? 'calc(50% + 120px)' : 'calc(50% - 60px)');
     }
 
 }
