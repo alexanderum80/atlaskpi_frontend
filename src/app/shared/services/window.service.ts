f7ff7de734021edfe24e5a1aab9b663fb2b4870a
Orlando Quero <orlaqp@gmail.com>
2019-01-30 16:06:34 -0500

    fixed issue with notifications navigation

--- src/app/shared/services/window.service.ts
+++ src/app/shared/services/window.service.ts
@@ -1,5 +1,5 @@
 import { Router } from '@angular/router';
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 
 function getWindow (): any {
     return window;
@@ -8,12 +8,17 @@ function getWindow (): any {
 @Injectable()
 export class WindowService {
 
-    constructor(private _router: Router) {
+    constructor(
+        private _router: Router,
+        private _zone: NgZone,
+    ) {
 
         // add navigation method to accesable directly from the window object
         getWindow().atlasKpi = {
             navigate: function(url: string) {
-                _router.navigateByUrl(url);
+                _zone.run(() => {
+                    _router.navigate([`${url}`]);
+                });
             }
         };
 
